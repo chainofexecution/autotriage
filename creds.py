7380a495466f44f8d4189c88b1568dbdb15511e0
Ian Moyer <92492482+chainofexecution@users.noreply.github.com>
2024-02-02 18:20:46 -0500

    Updated resume

--- creds.py
+++
@@ -1,5 +0,0 @@
-fixtUsername = "VHN0ZXJsaTg=" # base64 only, no plaintext!
-fixtMagicWord = "RnVja3RoaXMkaGl0MQ==" # base64 only, no plaintext!
-rqUsername = "aWFuLm1veWVyQGFzc3VyYW50LmNvbQ==" # base64 only, no plaintext!
-rqMagicWord = "MDIzODkxOTc=" # base64 only, no plaintext!
-assignedTech = "TW95ZXIsIElhbg==" # base64 only, no plaintext!
